49803266762f7f3d01bcf03c57a272419ae83937
non1414 <nouf22228@gmail.com>
2023-05-17 01:41:39 +0300

    Initial Commit

---
+++ SelfCheck-in/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SelfCheck-in
+//
+//  Created by نوف بخيت الغامدي on 27/10/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SelfCheck-in/SelfCheck_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfCheck_inApp.swift
+//  SelfCheck-in
+//
+//  Created by نوف بخيت الغامدي on 27/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SelfCheck_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
