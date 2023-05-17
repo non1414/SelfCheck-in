65be70af7e7be5f51e3da2467b6cb486e4758e40
non1414 <nouf22228@gmail.com>
2023-05-17 11:51:29 +0300

    First Push

--- SelfCheck-in/SelfCheck_inApp.swift
+++ SelfCheck-in/SelfCheck_inApp.swift
@@ -4,14 +4,27 @@
 //
 //  Created by نوف بخيت الغامدي on 27/10/1444 AH.
 //
-
+//First Push
 import SwiftUI
+
+import FirebaseCore
 
 @main
 struct SelfCheck_inApp: App {
+ 
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+    
+  }
 }
